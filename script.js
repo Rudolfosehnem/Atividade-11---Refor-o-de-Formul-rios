e754e3ad73d845321539f002a933b428382a04eb
Rudolfo <rudolfo_silva@estudante.sesisenai.org.br>
2025-04-15 07:30:12 -0300

    update-1

--- script.js
+++
@@ -1,9 +0,0 @@
-function logar(){
-
-    var login = document.getElementById('login').value;
-    var senha = document.getElementById('senha').value;
-
-    if( login == "admin" && senha == "admin"){
-        alert('sucesso');
-
-}
